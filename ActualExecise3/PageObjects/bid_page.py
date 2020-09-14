f6e47ba73791b002a8b464d2b0cd5f158296ab88
Huliyuanhly <1076215213@qq.com>
2020-09-14 23:16:42 +0800

    bid

---
+++ ActualExecise3/PageObjects/bid_page.py
@@ -0,0 +1,31 @@
+from selenium.webdriver.remote.webdriver import WebDriver  # driver来历
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from ActualExecise3.PageLocators.bid_page_loc import BidPageLoc as loc
+
+
+class BidPage:
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    # 获取用户余额
+    def get_user_money(self):
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(loc.money_input))
+        return self.driver.find_element(*loc.money_input).get_attribute("data-amount")
+
+    # 获取标的余额
+    def get_bid_money(self):
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(loc.bid_left_money_text))
+        return self.driver.find_element(*loc.bid_left_money_text).text
+
+    # 投标
+    def invest(self, invest_amount):
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(loc.invest_button))
+        self.driver.find_element(*loc.money_input).send_keys(invest_amount)
+        self.driver.find_element(*loc.invest_button).click()
+
+    # 点击 投资成功提示框中的 查看并激活
+    def click_active_button_in_success_popup(self):
+        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(loc.pop_up_dialog_after_invest_success))
+        self.driver.find_element(*loc.pop_up_dialog_after_invest_success).click()
+
---
+++ ActualExecise3/TestDatas/Common_datas.py
@@ -0,0 +1,13 @@
+import os
+
+# base_url
+_base_url = "http://120.78.128.25:8765/"
+# 登录地址
+login_url = os.path.join(_base_url, "index/login.html")
+# print(login_url)
+# 首页
+home_url = os.path.join(_base_url, "Index/index")
+# print(home_url)
+# 普通账号
+user = "18684720553"
+passwd = "python"
